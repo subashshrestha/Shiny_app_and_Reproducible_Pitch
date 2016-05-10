cb531559432512c097e708dccda2a372e229e6be
Subash Shrestha <info@subashstha.com.np>
2016-05-10 18:16:29 +0545

    final commited

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-# Tourist Visit to Lumbini
+# Developing Data Products - Course Project - Part 1
 
-This is my first attempt to create Shiny Application. I decided to create an interactive visualization to present number of tourists from different countries visiting Lumbini, Nepal in year 2012, 2013 and 2014. I have downloaded data provided by [Open Data Nepal](http://data.opennepal.net/content/number-foreigner-lumbini-2012-2013). This data file contains the information of number of tourists visiting Lumbini on year 2012, 2013, 2014.
+This repository has the ui.R and server.R files for the first part of the Developing Data Products Course Project. The peer assessed assignment has two parts. First, we need to create a Shiny application and deploy it on Rstudio's servers. Second, we should use Slidify or Rstudio Presenter to prepare a reproducible pitch presentation about the application.
 
-Years on Data 
-```
-2012 2013 2014 (AD)
-```  
-Number of Countries in data : `57`
+The app developed for the first part of the assignment is avalilable at:
+https://subashshrestha.shinyapps.io/Shiny_app_and_Reproducible_Pitch/
 
-Months for which data is recorded
-```
-January, February, March, April, May, June, July, 
-August, September, October, November, December
-```
+The presentation is available at: http://rpubs.com/subashshrestha/DataProduct_Slidify
+
+Files for the presentation are avalilable on the GitHub: https://github.com/subashshrestha/Shiny_app_and_Reproducible_Pitch
 
 You can view the [shiny apps](https://subashshrestha.shinyapps.io/Shiny_app_and_Reproducible_Pitch/) deployed. 
 
--- server.R
+++ server.R
@@ -1,59 +1,28 @@
-#Load Required Library
-library(dygraphs)
-library(ggplot2)
-library(reshape2)
-library(xts)
+# Coursera - Developing Data Products- Course Project
 
-#Load data
-tourist <- read.csv('tourist.csv', stringsAsFactors = FALSE)
-tourist <- tourist[,c(1,3,4,5)]
-tourist$No.of.Visitors <- as.numeric(tourist$No.of.Visitors)
-tourist$Year.AD <- as.factor(tourist$Year.AD)
-tourist$Month <- factor(tourist$Month
-                        , levels = c("Jan.","Feb.","Mar.","April","May","June","July",
-                                     "Aug.","Sept.","Oct.","Nov.","Dec.")
-                        , labels = c("01","02","03","04","05","06","07",
-                                     "08","09", "10","11","12"))
+# server.R file for the shiny app
 
-#Identify top 5 countries with highest number of visitors to Nepal
-aggregated_data <- aggregate(No.of.Visitors ~ Country, data = tourist, FUN = sum)
-top_5_country <- aggregated_data[order(-aggregated_data$No.of.Visitors),][1:5,]
-top_5_country$Country <- factor(top_5_country$Country, levels = top_5_country$Country)
+# This app was developed to help people choose the best car for their trip, 
+# using mtcars dataset, from [R]
 
-shinyServer(
-        function(input, output) {
-                datasetInput <- reactive({
-                        dataset <- subset(tourist, Country == input$Country)
-                        dataset
-                })
-                
-                output$Country  <- renderPrint({input$Country})
-                output$dygraph <- renderDygraph({
-                        touristData <- datasetInput()
-                        tourist_xts <- as.xts(
-                                touristData[4]
-                                , order.by = as.Date(
-                                        paste0(touristData$Year.AD,"-",touristData$Month,
-                                               "-01",format = "%Y-%M-01")
-                                )
-                        )
-                        dygraph(data = tourist_xts, 
-                                main = paste("Tourist Visit from ",input$Country,
-                                             " to Lumbini, Nepal", sep = "")) %>% 
-                                dyRangeSelector()
-                })
-                output$view <- renderPlot({
-                        ggplot(top_5_country, aes(x = Country, y = No.of.Visitors)) + 
-                                geom_bar(stat = "identity", fill = "red") + 
-                                theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-                                ylab("Number of Visitors")
-                })
-                output$table_output <- renderTable({
-                     dataset <- datasetInput()
-                     dataset <- aggregate(No.of.Visitors ~ Year.AD, 
-                                          data = dataset, FUN = sum)
-                     dataset
-                })
-        }
-)
+library(shiny)
+library(datasets)
+library(dplyr)
 
+shinyServer(function(input, output) {
+
+    # Show the cars that correspond to the filters
+    output$table <- renderDataTable({
+        disp_seq <- seq(from = input$disp[1], to = input$disp[2], by = 0.1)
+        hp_seq <- seq(from = input$hp[1], to = input$hp[2], by = 1)
+        data <- transmute(mtcars, Car = rownames(mtcars), MilesPerGallon = mpg, 
+                          GasolineExpenditure = input$dis/mpg*input$cost,
+                          Cylinders = cyl, Displacement = disp, Horsepower = hp, 
+                          Transmission = am)
+        data <- filter(data, GasolineExpenditure <= input$gas, Cylinders %in% input$cyl, 
+                       Displacement %in% disp_seq, Horsepower %in% hp_seq, Transmission %in% input$am)
+        data <- mutate(data, Transmission = ifelse(Transmission==0, "Automatic", "Manual"))
+        data <- arrange(data, GasolineExpenditure)
+        data
+    }, options = list(lengthMenu = c(5, 15, 30), pageLength = 30))
+})
